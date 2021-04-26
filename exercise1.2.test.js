feb97c957b60f00a8153a28516f5e17289838646
ElviraCher <ellisha25sheri@gmail.com>
2021-04-26 23:15:23 +0300

    Create test for exercise 1.2

---
+++ exercise1.2.test.js
@@ -0,0 +1,6 @@
+const getStringsLength = require('./exercise1.2');
+
+it('string length "JavaScript developer" + "Basic" to equal 25', () => {
+    expect(getStringsLength("JavaScript developer", "Basic")).toBe(25);
+});
+
