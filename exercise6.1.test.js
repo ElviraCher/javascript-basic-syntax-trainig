169f869c23588f49bd9dbfaae745aa19542c9b64
ElviraCher <ellisha25sheri@gmail.com>
2021-04-27 20:28:30 +0300

    Create test for exercise6.1

---
+++ exercise6.1.test.js
@@ -0,0 +1,8 @@
+const diff = require('./exercise6.1');
+
+it('return difference between higher and lower number' , () => {
+    expect(diff(6, 8)).toBe(2);
+    expect(diff(-3, 5)).toBe(8);
+    expect(diff(7, 7)).toBe(0);
+});
+
