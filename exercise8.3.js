8762b0b4d9a2cce4b548e4801d1033b54f067a31
ElviraCher <ellisha25sheri@gmail.com>
2021-04-27 23:45:16 +0300

    Create exercise8.3

---
+++ exercise8.3.js
@@ -0,0 +1,12 @@
+function findYoungerPerson(date1, date2) {
+    const person1Birthday = new Date(date1);
+    const person2Birthday = new Date(date2);
+
+    if (person2Birthday.getTime() < person1Birthday.getTime()) {
+        return date1;
+    } else {
+        return date2;
+    }
+}
+
+module.exports = findYoungerPerson;
