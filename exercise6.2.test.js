3d9636a6c1ae3ad6533677202ea5c6b650bd424b
ElviraCher <ellisha25sheri@gmail.com>
2021-04-27 20:44:35 +0300

    Create test for exercise6.2

---
+++ exercise6.2.test.js
@@ -0,0 +1,13 @@
+const isWord = require('./exercise6.2');
+
+it('word without space to be true word', () => {
+    expect(isWord('mother')).toBeTruthy();
+    expect(isWord('JavaScript')).toBeTruthy();
+});
+
+it('some words with space between it to be false, because it is not word', () => {
+    expect(isWord('JavaScript Basic')).toBeFalsy();
+    expect(isWord('I want to sleep')).toBeFalsy();
+});
+
+
