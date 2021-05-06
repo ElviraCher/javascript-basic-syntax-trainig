25e3f485c981b5a87435571aaa81ad7c7db6f19a
ElviraCher <ellisha25sheri@gmail.com>
2021-05-06 23:29:06 +0300

    Fix tests for exercise1.2

--- exercise1.2.test.js
+++ exercise1.2.test.js
@@ -1,5 +1,17 @@
 import { getStringsLength } from "./exercise1.2";
 
-it('string length "JavaScript developer" + "Basic" to equal 25', () => {
-  expect(getStringsLength("JavaScript developer", "Basic")).toBe(25);
+describe("getStringsLength", () => {
+  it("is a function", () => {
+    expect(getStringsLength).toBeInstanceOf(Function);
+  });
+  const strings = [
+    ["my cat", "is the best in the whole world", 36],
+    ["London", "is the capital of Great Britain", 37],
+    ["my gorgeous", "revolver", 19],
+  ];
+  strings.forEach(([str1, str2, result]) => {
+    it(`strings length: ${str1} + ${str2} is ${result}`, () => {
+      expect(getStringsLength(str1, str2)).toBe(result);
+    });
+  });
 });
