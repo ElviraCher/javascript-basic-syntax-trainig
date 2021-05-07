f93488812e8247ff7d2546addf842a27ca0885e4
ElviraCher <ellisha25sheri@gmail.com>
2021-05-07 13:00:52 +0300

    Fix tests for exercise6.3

--- exercise6.3.test.js
+++ exercise6.3.test.js
@@ -1,14 +1,19 @@
 import { pow } from "./exercise6.3";
 
-const values = [
-  [2, 3, 8],
-  [3, 3, 27],
-  [3, 2, 9],
-  [15, 1, 15],
-];
+describe("pow", () => {
+  it("is a function", () => {
+    expect(pow).toBeInstanceOf(Function);
+  });
 
-values.forEach(([a, x, result]) => {
-  it(`number ${a} in pow ${x} is ${result}`, () => {
-    expect(pow(a, x)).toEqual(result);
+  const values = [
+    [2, 3, 8],
+    [3, 3, 27],
+    [3, 2, 9],
+    [15, 1, 15],
+  ];
+  values.forEach(([a, x, result]) => {
+    it(`number ${a} in pow ${x} is ${result}`, () => {
+      expect(pow(a, x)).toEqual(result);
+    });
   });
 });
