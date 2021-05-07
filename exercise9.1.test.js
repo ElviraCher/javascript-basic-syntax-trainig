6b7d038e485f1c130dbfa3bd325c8f58b7eb5d51
ElviraCher <ellisha25sheri@gmail.com>
2021-05-07 15:41:48 +0300

    Fix tests for exercise9.1

--- exercise9.1.test.js
+++ exercise9.1.test.js
@@ -1,25 +1,29 @@
 import { isTriangleRight } from "./exercise9.1";
 
-const sidesRight = [
-  [3, 4, 5],
-  [10, 8, 6],
-  [24, 18, 30],
-];
-
-sidesRight.forEach(([a, b, c]) => {
-  it(`triangle with sides ${a}, ${b} and ${c} is right`, () => {
-    expect(isTriangleRight(a, b, c)).toBeTruthy();
+describe("isTriangleRight", () => {
+  it("is a function", () => {
+    expect(isTriangleRight).toBeInstanceOf(Function);
   });
-});
 
-const sidesOther = [
-  [4, 4, 5],
-  [9, 8, 6],
-  [24, 18, 31],
-];
+  const sidesRight = [
+    [3, 4, 5],
+    [10, 8, 6],
+    [24, 18, 30],
+  ];
+  sidesRight.forEach(([a, b, c]) => {
+    it(`triangle with sides ${a}, ${b} and ${c} is right`, () => {
+      expect(isTriangleRight(a, b, c)).toBeTruthy();
+    });
+  });
 
-sidesOther.forEach(([a, b, c]) => {
-  it(`triangle with sides ${a}, ${b} and ${c} is not right`, () => {
-    expect(isTriangleRight(a, b, c)).toBeFalsy();
+  const sidesOther = [
+    [4, 4, 5],
+    [9, 8, 6],
+    [24, 18, 31],
+  ];
+  sidesOther.forEach(([a, b, c]) => {
+    it(`triangle with sides ${a}, ${b} and ${c} is not right`, () => {
+      expect(isTriangleRight(a, b, c)).toBeFalsy();
+    });
   });
 });
