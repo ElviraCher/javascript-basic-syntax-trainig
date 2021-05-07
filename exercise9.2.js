3cd291921f2cf792e7e3442066b73606ab5cae99
ElviraCher <ellisha25sheri@gmail.com>
2021-05-07 16:46:15 +0300

    Change the way of export function

--- exercise9.2.js
+++ exercise9.2.js
@@ -1,8 +1,6 @@
-function getCircleLengthAndSquare(r) {
+export function getCircleLengthAndSquare(r) {
   const radius = parseInt(r, 10);
   const circleLength = 2 * Math.PI * radius;
   const square = Math.PI * radius ** 2;
   return [circleLength.toFixed(2), square.toFixed(2)];
 }
-
-module.exports = getCircleLengthAndSquare;
