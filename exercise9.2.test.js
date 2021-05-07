f7aa655ed6f82925322f8475a455340c1eb7b842
ElviraCher <ellisha25sheri@gmail.com>
2021-05-07 16:45:37 +0300

    Fix tests

--- exercise9.2.test.js
+++ exercise9.2.test.js
@@ -1,13 +1,19 @@
-const getCircleLengthAndSquare = require("./exercise9.2");
+import { getCircleLengthAndSquare } from "./exercise9.2";
 
-const values = [
-  ["15", ["94.25", "706.86"]],
-  ["18", ["113.10", "1017.88"]],
-  ["6", ["37.70", "113.10"]],
-];
+describe("getCircleLengthAndSquare", () => {
+  it("is a function", () => {
+    expect(getCircleLengthAndSquare).toBeInstanceOf(Function);
+  });
 
-values.forEach(([radius, lengthAndSquare]) => {
-  it(`circle with radius ${radius} has length ${lengthAndSquare[0]} and square ${lengthAndSquare[1]}`, () => {
-    expect(getCircleLengthAndSquare(radius)).toEqual(lengthAndSquare);
+  const values = [
+    ["15", ["94.25", "706.86"]],
+    ["18", ["113.10", "1017.88"]],
+    ["6", ["37.70", "113.10"]],
+  ];
+  values.forEach(([radius, lengthAndSquare]) => {
+    it(`circle with radius ${radius} has length
+     ${lengthAndSquare[0]} and square ${lengthAndSquare[1]}`, () => {
+      expect(getCircleLengthAndSquare(radius)).toEqual(lengthAndSquare);
+    });
   });
 });
