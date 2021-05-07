2c8afd95902e25c2519e0714801c485684c41db3
ElviraCher <ellisha25sheri@gmail.com>
2021-05-07 15:27:11 +0300

    Fix tests for exercise8.1

--- exercise8.1.test.js
+++ exercise8.1.test.js
@@ -1,15 +1,20 @@
 import { getDayOfWeek } from "./exercise8.1";
 
-const datesAndDays = [
-  ["27.04.2020", "Monday"],
-  ["09.03.1994", "Wednesday"],
-  ["31.12.2020", "Thursday"],
-  ["16.06.2013", "Sunday"],
-  ["08.04.2008", "Tuesday"],
-];
+describe("getDayOfWeek", () => {
+  it("is a function", () => {
+    expect(getDayOfWeek).toBeInstanceOf(Function);
+  });
 
-datesAndDays.forEach(([input, dayOfWeek]) => {
-  it(`${input} is ${dayOfWeek}`, () => {
-    expect(getDayOfWeek(input)).toBe(dayOfWeek);
+  const datesAndDays = [
+    ["27.04.2020", "Monday"],
+    ["09.03.1994", "Wednesday"],
+    ["31.12.2020", "Thursday"],
+    ["16.06.2013", "Sunday"],
+    ["08.04.2008", "Tuesday"],
+  ];
+  datesAndDays.forEach(([input, dayOfWeek]) => {
+    it(`${input} is ${dayOfWeek}`, () => {
+      expect(getDayOfWeek(input)).toBe(dayOfWeek);
+    });
   });
 });
