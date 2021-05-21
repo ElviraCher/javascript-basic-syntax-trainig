6442889a92a0b98281cb1134173ad9aec60074ca
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-05-21 13:11:36 +0300

    Fix exercise9.1

--- exercise9.1.js
+++ exercise9.1.js
@@ -1,15 +1,4 @@
 export function isTriangleRight(a, b, c) {
-  const max = Math.max(a, b, c);
-  const min = Math.min(a, b, c);
-  let third;
-
-  if (a !== min && a !== max) {
-    third = a;
-  } else if (b !== min && b !== max) {
-    third = b;
-  } else if (c !== min && c !== max) {
-    third = c;
-  }
-
-  return max ** 2 === min ** 2 + third ** 2;
+  const arr = [a, b, c].sort((x, y) => (x - y));
+  return arr[2] ** 2 === arr[0] ** 2 + arr[1] ** 2;
 }
